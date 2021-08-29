5c96e26d23acaababb0435e77566ec07c626e7da
TaraMKelly <tara.kelly16@gmail.com>
2021-08-29 13:02:26 -0400

    commit tree 1b4ba6e2a8a9ac36eb9a7cbf8f2d08cb2e047a8f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name:"Tara", last_name:"Kelly", grade:98)
+Student.create!(first_name:"Naima", last_name:"Pohler", grade:100)
+Student.create!(first_name:"Alex", last_name:"Dumes", grade:95)
